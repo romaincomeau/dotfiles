f76bb42f065ccda88513a2bd233c2e2721d65d86
Romain Comeau <comeauromain@gmail.com>
2024-10-01 10:20:08 -0400

    updated theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-	"rebelot/kanagawa.nvim", 
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd([[colorscheme kanagawa]])
-	end,
+  "rose-pine/neovim", 
+  name = "rose-pine",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme rose-pine-moon]])
+  end,
 }
